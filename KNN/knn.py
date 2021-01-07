a07884bee6157c997be84ce99f129d24399cbd94
Saurabh Tiwari <33330452+meispi@users.noreply.github.com>
2021-01-07 16:54:33 +0530

    Update knn.py

--- KNN/knn.py
+++ KNN/knn.py
@@ -7,17 +7,15 @@ Created on Thu Jan  7 16:04:43 2021
 
 import pandas as pd
 
-dataset = pd.read_csv('C:\\Users\\hp\\Desktop\\Monu_Folder\\Python\\KNN\\heart.csv')
+dataset = pd.read_csv('') #file location
 
-dataset.head()
-
-x = dataset.iloc[:,:-1].values
-y = dataset.iloc[:,13].values
+x = dataset.iloc[:,:-1].values 
+y = dataset.iloc[:,13].values #replace 13 with last column of the dataset with 0 indexing
 
 from sklearn.model_selection import train_test_split
-x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.20, random_state=42)
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.20, random_state=42) #this resulted in the highest accuracy of 90% I have to find out why?
 
-from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import StandardScaler #the next 3 lines of code normalizes the dataset to produce more accurate results, why?
 scaler = StandardScaler()
 scaler.fit(x_train)
 
